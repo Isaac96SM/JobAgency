cdbff68fa58b58d5345d815c2b8d70b50badd3db
trisquel11 <agustincorsetti@gmail.com>
2016-12-16 16:29:59 +0100

    Update JobAgencyDB Script.sql

--- JobAgencyDB Script.sql
+++ JobAgencyDB Script.sql
@@ -1,27 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 15-12-2016 a las 17:12:24
--- Versión del servidor: 10.1.16-MariaDB
--- Versión de PHP: 7.0.9
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `jobagencydb`
 --
 
 -- --------------------------------------------------------
-
+create database jobagencydb;
+use jobagencydb;
 --
 -- Estructura de tabla para la tabla `companies`
 --
@@ -180,6 +164,4 @@ ALTER TABLE `usersoffers`
   ADD CONSTRAINT `usersoffers_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`),
   ADD CONSTRAINT `usersoffers_ibfk_2` FOREIGN KEY (`OfferID`) REFERENCES `offers` (`OfferID`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
